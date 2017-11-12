5a5dd4f8826cd0e97036548179984566892c1b78
Stephanie McSpedon-Marx <s.mcspedon@gmail.com>
2017-11-12 17:12:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Code your solutions in this file
-function printBadges(names) {
-  for (let i = 0; i < names.length; i++) {
-  console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
+function printBadges(arrayNames) {
+  arrayNames = ["Avi", "Grace", "Alan"]
+  
+  for (let i = 0; i < arrayNames.length; i++) {
+  console.log(`Welcome ${arrayNames[i]}! You are employee #${i+1}.`);
   }
   return names;
 }
