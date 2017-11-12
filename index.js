fa613b8693e22af2e3c734d9cbfbde390a6c7cba
Stephanie McSpedon-Marx <s.mcspedon@gmail.com>
2017-11-12 17:13:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 // Code your solutions in this file
 function printBadges(arrayNames) {
   arrayNames = ["Avi", "Grace", "Alan"]
-  
+
   for (let i = 0; i < arrayNames.length; i++) {
   console.log(`Welcome ${arrayNames[i]}! You are employee #${i+1}.`);
   }
-  return names;
-}
-
-function tailsNeverFails() {
-  let tails= 0;
-  while(Math.random() >= 0.5) {
-    tails ++;
-  }
-  return `You got &{tails} tails in a row!`;
+  return arrayNames;
 }
